55cfb95f544679e576abad59acaceb46196d895a
gsarankumar <gsarankumar@gmail.com>
2017-03-23 11:56:50 +0530

    Add files via upload

---
+++ stringoccurance.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class stringoccurance {
+	
+	public static void main(String[] args)throws Exception{
+	StringBuilder sb=new StringBuilder(System.in);
+	}
+	char ch1[];
+
+	}
+	
+}
+
+
+
+class a{
+	static int a;
+	static{
+		
+		a=20;
+		System.out.println(a);
+	}
+
+	
+}
